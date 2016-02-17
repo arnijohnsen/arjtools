349d08cf9939ed12608f4eccc71c34106bf13447
Árni Johnsen <arni.johnsen@gmail.com>
2016-02-17 14:22:13 +0000

    Added function to compute WAAI

--- R/waai_caai.R
+++ R/waai_caai.R
@@ -1,2 +1,14 @@
-waai <- function(){
+#' Compute whole arm abberation index (waai) for a chromosome arm
+#'
+#' @param seg_values Numeric vector, with values associated with segments from one chromosome arm (usually mean log ratio)
+#' @param seg_nprobes Numeric vector, with number of probes in each segment
+#' @return Whole arm abberation index for the given chromosome arm
+#' @examples
+#' # Read aCGH data to data.frame dat
+#' seg <- copynumber::pcf(dat)
+#' seg.dt <- data.table::data.table(seg)
+#' seg.dt[,.(waai = waai(mean, n.probes)), by=.(chrom, arm)]
+#' @export
+waai <- function(seg_values, seg_nprobes){
+  return(weighted.mean(seg_values, seg_nprobes, na.rm = T))
 }
