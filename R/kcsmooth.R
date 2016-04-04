1fd2871a5805395b9118f1a11ef7fb30b01ec0a7
Árni Johnsen <arni.johnsen@gmail.com>
2016-04-04 14:50:57 +0000

    Added grouping parameter to kcsmooth.

--- R/kcsmooth.R
+++ R/kcsmooth.R
@@ -12,35 +12,51 @@
 #' @param maxmem This parameter controls memory usage, set to lower value to lower memory consumption
 #' @param what character, determines what data should be returned. "pos" for only positive KCscore,
 #'   "neg" for only negative KCscore, "both" for both positive and negative KCscore.
+#' @param groups Numeric vector. If NULL is passed (default), each sample is smoothed seperately.
+#'   If a numeric vector is passed, it indicates which samples should be grouped together before
+#'   smoothing. For example, groups = c(1,1,2) indicates that the first two samples will be grouped
+#'   before smothing and the third will be smoothed seperately.
+#'   If a non-NULL value is passed, the return data frame will have columns equal to the number of groups passed.
+#'   (This is the same as passing group = c(1,2,3,...)).
 #' @return data.table with smoothed values. First column is chromosome, second column indicates if value is from positive
-#'   or negative smoothed values. All other columns are smoothed values for each sample.
+#'   or negative smoothed values. All other columns are smoothed values for each sample or group.
 #' @import data.table
 #' @export
-kcsmooth <- function(dt, mirrorLocs, sigma = 1e+06, sampleDensity = 50000, maxmem = 1000, verbose = T, what = "both"){
+kcsmooth <- function(dt, mirrorLocs, sigma = 1e+06, sampleDensity = 50000, maxmem = 1000, verbose = T, what = "both", groups= NULL){
   if(!(what %in% c("pos", "neg", "both"))){
     stop("what must be pos, neg or both")
   }
+  n <- dim(dt)[2]
+  if(is.null(groups)){
+    groups <- 1:(n-2)
+  }
+  n_groups <- length(unique(groups))
   spm_to_dt <- function(spm, what){
     spm_unlist <- unlist(spm@data)
     dt <- data.table(
-      chrom = str_replace(names(spm_unlist), "\\..*", ""),
-      posneg  = str_extract(names(spm_unlist), "[a-z]+"),
+      chrom = stringr::str_replace(names(spm_unlist), "\\..*", ""),
+      posneg  = stringr::str_extract(names(spm_unlist), "[a-z]+"),
       value = spm_unlist)
     if(what != "both"){
       dt <- dt[posneg == what]
     }
     return(dt)
   }
-  # kcsmooth single data
-  spm <- KCsmart::calcSpm(dt[,c(1:2, 3), with = F], hsMirrorLocs, sigma, sampleDensity, verbose)
+  # Smooth first group
+  spm <- KCsmart::calcSpm(dt[,c(1:2, which(groups == 1)+2), with = F], hsMirrorLocs, sigma, sampleDensity, verbose)
   all_dt <- spm_to_dt(spm, what)
-  n <- dim(dt)[2]
-  if (n > 3){
-    for(i in 4:n){
-      spm <- KCsmart::calcSpm(dt[,c(1:2, i), with = F], hsMirrorLocs, sigma, sampleDensity, verbose)
-      all_dt[,i:=spm_to_dt(spm, what)$value]
+  # Smooth groups 2, 3, ...
+  if (n_groups > 1){
+    for(i in 2:n_groups){
+      spm <- KCsmart::calcSpm(dt[,c(1:2, which(groups == i)+2), with = F], hsMirrorLocs, sigma, sampleDensity, verbose)
+      all_dt[,paste("col", i, sep = ""):=spm_to_dt(spm, what)$value]
     }
   }
-  setnames(all_dt, c("chrom", "what", names(dt)[3:n]))
+
+  if(identical(groups, 1:(n-2))){
+    setnames(all_dt, c("chrom", "what", names(dt)[3:n]))
+  } else {
+    setnames(all_dt, c("chrom", "what", paste("group", 1:n_groups, sep = "")))
+  }
   return(all_dt)
 }
